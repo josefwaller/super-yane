b9e24c0e209bb3cf662f73309b44f05c11047e14
Josef Waller <josef@siriusapplications.com>
2025-07-23 22:33:39 -0700

    Implement SPC700 AND and AND1

--- spc700/src/processor.rs
+++ spc700/src/processor.rs
@@ -41,6 +41,12 @@ impl Processor {
         self.sr.c = c1 | c2;
         r
     }
+    fn and(&mut self, l: u8, r: u8) -> u8 {
+        let v = l & r;
+        self.sr.z = v == 0;
+        self.sr.n = (v & 0x80) != 0;
+        v
+    }
 
     /// Immediate addressing
     fn imm(&mut self, _bus: &mut impl HasAddressBus) -> u16 {
@@ -90,6 +96,16 @@ impl Processor {
     fn abs_y(&mut self, bus: &mut impl HasAddressBus) -> u16 {
         self.abs(bus).wrapping_add(self.y as u16)
     }
+    fn mb(&mut self, bus: &mut impl HasAddressBus) -> bool {
+        let value = u16::from_le_bytes([
+            bus.read(self.pc as usize),
+            bus.read(self.pc.wrapping_add(1) as usize),
+        ]);
+        let addr = value & 0x1FFF;
+        let bit = value >> 13;
+        let value = bus.read(addr as usize);
+        (value & (0x01 << bit)) != 0
+    }
     pub fn step(&mut self, bus: &mut impl HasAddressBus) {
         macro_rules! read_a_func {
             ($read: ident, $func: ident) => {{
@@ -123,6 +139,15 @@ impl Processor {
                 self.a = (result & 0xFF) as u8;
             }};
         }
+        macro_rules! read_bit_func {
+            ($flag: ident, $op: tt, $negate: expr) => {{
+                // Todo maybe: Move MB to a macro param
+                let bit = self.mb(bus);
+                let val = (bit $op self.sr.$flag);
+                self.sr.$flag = if $negate { !val } else { val };
+            }};
+            ($flag: ident, $op: tt) => {{ read_bit_func!($flag, $op, false) }};
+        }
         let opcode = bus.read(self.pc as usize);
         self.pc = self.pc.wrapping_add(1);
         match opcode {
@@ -139,6 +164,20 @@ impl Processor {
             ADC_D_D => read_write_func!(d, d, adc),
             ADC_D_IMM => read_write_func!(d, imm, adc),
             ADDW_YA_D => read_ya_func!(d, addw),
+            AND_IX_IY => read_write_func!(ix, iy, and),
+            AND_A_IMM => read_a_func!(imm, and),
+            AND_A_IX => read_a_func!(ix, and),
+            AND_A_IDY => read_a_func!(idy, and),
+            AND_A_IDX => read_a_func!(idx, and),
+            AND_A_D => read_a_func!(d, and),
+            AND_A_DX => read_a_func!(dx, and),
+            AND_A_ABS => read_a_func!(abs, and),
+            AND_A_ABSX => read_a_func!(abs_x, and),
+            AND_A_ABSY => read_a_func!(abs_y, and),
+            AND_D_D => read_write_func!(d, d, and),
+            AND_D_IMM => read_write_func!(d, imm, and),
+            AND1_C_NMB => read_bit_func!(c, &, true),
+            AND1_C_MB => read_bit_func!(c, &),
             _ => {}
         }
     }
