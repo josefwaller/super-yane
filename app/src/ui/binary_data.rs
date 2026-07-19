ffb06e924c934477c16f0e916d86ba95d4eeaecb
Josef Waller <josef@siriusapplications.com>
2026-07-18 18:39:34 -0700

    Convert binary graphic viewer to table

--- app/src/ui/binary_data.rs
+++ app/src/ui/binary_data.rs
@@ -1,6 +1,8 @@
 use egui::{
-    AtomExt, Color32, ColorImage, Image, ScrollArea, TextureHandle, TextureOptions, Ui, Vec2,
+    AtomExt, Color32, ColorImage, Image, Layout, Pos2, Rect, RichText, ScrollArea, TextureHandle,
+    TextureOptions, Ui, Vec2,
 };
+use egui_extras::{Column, TableBuilder};
 
 use crate::{ui::binary_table::binary_table, utils::bytes_to_rgb};
 
@@ -12,6 +14,39 @@ struct State {
 
 const TEXTURE_WIDTH_TILES: usize = 16;
 const TEXTURE_WIDTH_PIXELS: usize = 8 * TEXTURE_WIDTH_TILES;
+
+/// Update the texture, only updating the rows that the user has scrolled to right now.
+fn update_texture(
+    tile_row: usize,
+    height_tiles: usize,
+    data: &[u8],
+    palette: &[u16],
+    bpp: usize,
+    texture: &mut TextureHandle,
+) {
+    // Due to how tiles are structured in VRAM, we need to round down to the nearest tile (i.e 8 pixels)
+    let pixel_row = 8 * tile_row;
+    let total_height = 8 * height_tiles;
+    let bytes_per_tile = 8 * bpp;
+    let offset_byte = pixel_row / 8 * TEXTURE_WIDTH_TILES * bytes_per_tile;
+    // Todo: Not allocate every frame
+    let mut buf = vec![[0; 3]; TEXTURE_WIDTH_PIXELS * total_height];
+    // Get RGB data
+    bytes_to_rgb(
+        &data[offset_byte..],
+        16,
+        total_height / 8,
+        bpp,
+        palette,
+        &mut buf,
+    );
+    // Copy only section of texture
+    texture.set_partial(
+        [0, pixel_row],
+        ColorImage::from_rgb([TEXTURE_WIDTH_PIXELS, total_height], buf.as_flattened()),
+        TextureOptions::NEAREST,
+    );
+}
 
 pub fn binary_data(ui: &mut Ui, data: &[u8], color: Color32, palette: &[u16]) {
     ui.vertical(|ui| {
@@ -27,7 +62,6 @@ pub fn binary_data(ui: &mut Ui, data: &[u8], color: Color32, palette: &[u16]) {
         });
         ui.checkbox(&mut state.view_tiles, "View as tiles");
         let mut texture = state.texture.clone().unwrap_or_else(|| {
-            log::debug!("LOAD TEXTURE");
             ui.load_texture(
                 id.value().to_string(),
                 ColorImage::filled([TEXTURE_WIDTH_PIXELS, 0], Color32::RED),
@@ -37,7 +71,6 @@ pub fn binary_data(ui: &mut Ui, data: &[u8], color: Color32, palette: &[u16]) {
         state.texture = Some(texture.clone());
         // Ensure texture size is correct
         if texture.size() != [TEXTURE_WIDTH_PIXELS, texture_height] {
-            log::debug!("CHANGE SIZE");
             texture.set(
                 ColorImage::filled([TEXTURE_WIDTH_PIXELS, texture_height], Color32::RED),
                 TextureOptions::NEAREST,
@@ -46,34 +79,58 @@ pub fn binary_data(ui: &mut Ui, data: &[u8], color: Color32, palette: &[u16]) {
         ui.ctx().data_mut(|d| d.insert_persisted(id, state.clone()));
         // Show data
         if state.view_tiles {
-            ScrollArea::vertical()
-                .auto_shrink([false, false])
-                .show_viewport(ui, |ui, viewport| {
-                    // Render screen data onto texture
-                    let screen_pixel_per_pixel = viewport.width() / TEXTURE_WIDTH_PIXELS as f32;
-                    // Due to how tiles are structured in VRAM, we need to round down to the nearest tile (i.e 8 pixels)
-                    let offset_row =
-                        ((viewport.top() / screen_pixel_per_pixel).floor() as usize / 8) * 8;
-                    let height =
-                        ((viewport.height() / screen_pixel_per_pixel).ceil() as usize / 8 + 2) * 8;
-                    // Don't go past the last row
-                    let offset_row = offset_row.min(texture_height.saturating_sub(height));
-                    let bytes_per_tile = 8 * bpp;
-                    let offset_byte = offset_row / 8 * TEXTURE_WIDTH_TILES * bytes_per_tile;
-                    let mut buf = vec![[0; 3]; TEXTURE_WIDTH_PIXELS * height];
-                    bytes_to_rgb(&data[offset_byte..], 16, height / 8, bpp, palette, &mut buf);
-                    texture.set_partial(
-                        [0, offset_row],
-                        ColorImage::from_rgb([TEXTURE_WIDTH_PIXELS, height], buf.as_flattened()),
-                        TextureOptions::NEAREST,
-                    );
-                    let img = Image::from_texture((texture.id(), texture.size_vec2()))
-                        .fit_to_exact_size(Vec2 {
-                            x: ui.available_width(),
-                            y: f32::INFINITY,
-                        })
-                        .max_size(Vec2::INFINITY);
-                    ui.add(img);
+            // Rows should be square
+            let row_height = (ui.available_width() / TEXTURE_WIDTH_TILES as f32).floor();
+            TableBuilder::new(ui)
+                .column(Column::auto())
+                .columns(Column::remainder(), TEXTURE_WIDTH_TILES)
+                .cell_layout(Layout::centered_and_justified(egui::Direction::LeftToRight))
+                .header(18.0, |mut row| {
+                    row.col(|ui| {
+                        ui.label("IDX/ADDR");
+                    });
+                    for index in 0..TEXTURE_WIDTH_TILES {
+                        row.col(|ui| {
+                            ui.label(RichText::new(format!("+{:X}", index)).color(color));
+                        });
+                    }
+                })
+                // Each row is one tile (8 pixels)
+                .body(|body| {
+                    // Update only the section of the texture we are looking at
+                    body.rows(row_height, texture_height / 8, |mut row| {
+                        let index = row.index();
+                        // Update row texture
+                        update_texture(index, 1, data, palette, bpp, &mut texture);
+                        row.col(|ui| {
+                            ui.label(
+                                RichText::new(format!(
+                                    "{:X}/{:04X}",
+                                    TEXTURE_WIDTH_TILES * index,
+                                    TEXTURE_WIDTH_TILES * index * 8 * bpp
+                                ))
+                                .color(color),
+                            );
+                        });
+                        let img_width = 1.0 / TEXTURE_WIDTH_TILES as f32;
+                        let img_height = 1.0 / (texture_height / 8) as f32;
+                        for x in 0..TEXTURE_WIDTH_TILES {
+                            row.col(|ui| {
+                                ui.painter().image(
+                                    texture.id(),
+                                    ui.available_rect_before_wrap(),
+                                    Rect::from_min_max(
+                                        Pos2::new(x as f32 * img_width, index as f32 * img_height),
+                                        Pos2::new(
+                                            (x + 1) as f32 * img_width,
+                                            (index + 1) as f32 * img_height,
+                                        ),
+                                    ),
+                                    Color32::WHITE,
+                                );
+                            });
+                        }
+                    })
                 });
         } else {
             binary_table(ui, data, color);
